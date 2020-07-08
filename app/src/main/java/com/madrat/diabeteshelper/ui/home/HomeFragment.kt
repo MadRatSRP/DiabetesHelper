334510088925dffd9a509e058b6a6d94a8534ae1
MadRat <mischa.alpeew@yandex.ru>
2020-07-08 19:02:35 +0300

    Начал реализацию модуля для учебной практики Внедрил базовые компоненты Dropbox Java SDK, в следующем коммите исправлю ошибки, связанные с его использованием

--- app/src/main/java/com/madrat/diabeteshelper/ui/home/HomeFragment.kt
+++ app/src/main/java/com/madrat/diabeteshelper/ui/home/HomeFragment.kt
@@ -8,17 +8,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.dropbox.core.DbxRequestConfig
+import com.dropbox.core.v2.DbxClientV2
 import com.madrat.diabeteshelper.R
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
@@ -26,6 +25,17 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val dropboxAccessToken = context?.getString(R.string.dropbox_access_token)
+
+        val config = DbxRequestConfig
+            .newBuilder("jopka")
+            .build()
+        val client = DbxClientV2(config, dropboxAccessToken)
+
+        val fullAcount = client.users().currentAccount
+        println(fullAcount.name.displayName)
+
         return root
     }
 }
