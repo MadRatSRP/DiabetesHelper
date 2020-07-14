e294c07a31f8d28f4141f43d8f585883cc59b5e4
MadRat96 <mischa.alpeew@yandex.ru>
2020-07-14 17:57:25 +0300

    Удалил файлы, относящиеся к другим фрагментам и перенёс FAB во фрагмент Home

--- app/src/main/java/com/madrat/diabeteshelper/ui/home/HomeFragment.kt
+++ app/src/main/java/com/madrat/diabeteshelper/ui/home/HomeFragment.kt
@@ -4,33 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.dropbox.core.DbxRequestConfig
-import com.dropbox.core.v2.DbxClientV2
-import com.dropbox.core.v2.files.FileMetadata
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.madrat.diabeteshelper.HomeMVP
 import com.madrat.diabeteshelper.HomePresenter
 import com.madrat.diabeteshelper.HomeRepository
 import com.madrat.diabeteshelper.R
 
 class HomeFragment: Fragment(), HomeMVP.View {
-
-    private lateinit var homeViewModel: HomeViewModel
-
     private var presenter: HomePresenter? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val floatingActionButton: FloatingActionButton = root.findViewById(R.id.fab)
+
+        floatingActionButton.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
         return root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
