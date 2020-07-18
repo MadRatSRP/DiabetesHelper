152f8cb360612854cb268b75e8f067a95e497122
MadRat96 <mischa.alpeew@yandex.ru>
2020-07-18 14:42:28 +0300

    Подключил Gson, заменю код Moshi на Gson

--- app/src/main/java/com/madrat/diabeteshelper/logic/Home.kt
+++ app/src/main/java/com/madrat/diabeteshelper/logic/Home.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Home(
+class Home(
     val author: String,
 
     val value: String
--- app/src/main/java/com/madrat/diabeteshelper/ui/imports/ImportFragment.kt
+++ app/src/main/java/com/madrat/diabeteshelper/ui/imports/ImportFragment.kt
@@ -12,14 +12,24 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.dropbox.core.DbxRequestConfig
 import com.dropbox.core.v2.DbxClientV2
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.madrat.diabeteshelper.R
 import com.madrat.diabeteshelper.databinding.FragmentImportBinding
 import com.madrat.diabeteshelper.hideKeyboardAndClearFocus
+import com.madrat.diabeteshelper.logic.Home
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.io.BufferedReader
+import java.io.InputStream
 
 class ImportFragment: Fragment() {
     // ViewBinding variables
@@ -60,6 +70,7 @@ class ImportFragment: Fragment() {
         }
     }
 
+    // Import from Dropbox
     private fun showSaveAndExportDialog() {
         val builder = AlertDialog.Builder(context)
         builder.setCancelable(true)
@@ -74,47 +85,56 @@ class ImportFragment: Fragment() {
 
         editText.hideKeyboardAndClearFocus {  }
 
-        var currentExtension = ""
+        var fileExtension = ""
 
         radioGroup.setOnCheckedChangeListener { _, radioButtonId ->
             when(radioButtonId) {
                 R.id.radio_csv -> {
-                    currentExtension = ".csv"
+                    fileExtension = ".csv"
                 }
                 R.id.radio_xml -> {
-                    currentExtension = ".xml"
+                    fileExtension = ".xml"
                 }
                 R.id.radio_json -> {
-                    currentExtension = ".json"
+                    fileExtension = ".json"
                 }
             }
         }
 
         setAndSaveButton.setOnClickListener {view->
-            val string = view.context.getString(
+            val fileName = view.context.getString(
                 R.string.pattern_filename,
                 editText.text.toString(),
-                currentExtension
+                fileExtension
             )
 
-            println(string)
-
-            getFileDisposable(
-                string
-            )
+            getFileDisposable(fileName, fileExtension)
         }
 
         val alertDialog = builder.create()
         alertDialog.show()
     }
-    private fun getFileDisposable(filePath: String): Disposable? {
+    private fun getFileDisposable(filePath: String,
+                                  fileExtension: String): Disposable? {
         return Observable.fromCallable {
             downloadFileFromServer(filePath)
         }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe{result ->
-                println(result)
+                when(fileExtension) {
+                    ".csv" -> {
+
+                    }
+                    ".xml" -> {
+                       // convertJsonToHomeObject(result)
+                    }
+                    ".json" -> {
+                        if (result != null) {
+                            deserializeJson(result)
+                        }
+                    }
+                }
             }
     }
     private fun downloadFileFromServer(filePath: String): String {
@@ -124,4 +144,24 @@ class ImportFragment: Fragment() {
             .bufferedReader()
             .use(BufferedReader::readText)
     }
+    fun deserializeJson(jsonString: String) {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        /*val listHome = Types.newParameterizedType(
+            List::class.java, Home::class.java
+        )*/
+
+        val adapter: JsonAdapter<Home> = moshi.adapter(Home::class.java)
+
+        val list = adapter.fromJson(jsonString)
+
+        println(list)
+    }
+    /*fun convertJsonToHomeObject(jsonString: String) {
+        val deserializedValue = jacksonObjectMapper().readerFor(Home::class.java).readValue<Home>(jsonString)
+
+        println(deserializedValue)
+    }*/
 }
